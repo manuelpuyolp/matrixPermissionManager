eb3ebf7efada4e0189d1545308a49e6f7b3b2e50
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-25 16:24:24 +0100

    Init

--- vars/util.groovy
+++ vars/util.groovy
@@ -53,6 +53,16 @@ def getJobConfig(String jobName) {
     return fileContent
 }
 
+def getCrumb(String user, String password, String downloadPath) {
+    sh "touch  ${downloadPath}/crumb.txt"
+    sh "curl -o ${downloadPath}/crumb.txt -X GET -u ${user}:${password} ${JENKINS_URL}crumbIssuer/api/xml"
+    def crumbFile = new File("${downloadPath}/crumb.txt")
+    def crumbFileCont = crumbFile.getText()
+    def parts = crumbFileCont.split("<crumb>")
+    def next = parts[1].split("</crumb>")
+    return next[0]
+}
+
 def printJob(String jobName) {
     String text = getJobConfig(jobName)
     println(text)
